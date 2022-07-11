0af761c94b7caf4426f85fa379216e1beafc95eb
manaswita <manaswita.s1@gmail.com>
2022-07-11 17:51:45 +0530

    Creating a Function to Play One Round and Display the Winner

--- play-script.js
+++ play-script.js
@@ -4,13 +4,34 @@ function computerPlay()
     return options[Math.floor(Math.random()*options.length)];
 }
 
-console.log(computerPlay());
-
-
 
 function caseInsensitive(s){
       return s.toLowerCase();
 }
 
 let playerSelection = caseInsensitive(prompt("Choose Rock /Paper/ Scissors"));
-console.log(playerSelection);
+
+
+let computerSelection = computerPlay();
+
+function playRound(playerSelection,computerSelection){
+    if((playerSelection==='rock' && computerSelection==='paper')|| (playerSelection==='paper' && computerSelection==='scissors') || (playerSelection==='scissors' && computerSelection==='rock'))
+    {
+        return "You Lose!"+ computerSelection+" beats "+playerSelection+" !";
+    }
+
+    else if((playerSelection==='rock' && computerSelection==='scissors') || (playerSelection==='paper' && computerSelection==='rock' )|| (playerSelection==='scissors' && computerSelection==='paper'))
+    {
+        return "You Win! "+playerSelection+" beats "+computerSelection+" !";
+    }
+       
+    else
+    {
+       return "It's a Tie !";
+    }
+}
+
+
+
+console.log(playRound(playerSelection,computerPlay()));
+
